bc2151606b693256fe5714bb44083ae4213e21d9
OlikoGorobei <101796357+OlikoGorobei@users.noreply.github.com>
2022-03-22 20:31:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BARSGroupHW1
+{
+    class Program
+    {
+
+        class Task
+        {
+            public event EventHandler <char>  OnKeyPressed;
+            public void Run()
+            {
+                var symbol = Console.ReadKey();
+                while (true)
+                {
+                    if (symbol.Key == ConsoleKey.C)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        OnKeyPressed?.Invoke(this, symbol.KeyChar);
+                    }
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task Subscriber = new Task();
+            EventHandler<char> OnKeyPressed = (sender, symbol) => Console.WriteLine($"веденный символ:", symbol);
+            Subscriber.Run();
+
+        }
+    }
+}
